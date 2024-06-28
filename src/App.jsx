ea5e9759907263fdc74ca2968bf8adf43c4dadb4
rchavezm <rchavez.tech@gmail.com>
2024-06-28 14:02:33 -0700

    firstcommit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Hero from './Components/Hero/Hero'
+import Programs from './Components/Programs/Programs'
+import Title from './Components/Title/Title'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <div className="container">
+        <Title />
+      <Programs />
+
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
