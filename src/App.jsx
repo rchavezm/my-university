369656a01c518cb72740a151c4cbad7451175626
rchavezm <rchavez.tech@gmail.com>
2024-07-01 12:56:46 -0700

    did form

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
 import Title from './Components/Title/Title'
 import About from './Components/About/About'
+import Campus from './Components/Campus/Campus'
+import Testimonials from './Components/Testimonials/Testimonials'
+import Contact from './Components/Contact/Contact'
 
 const App = () => {
   return (
@@ -14,6 +17,12 @@ const App = () => {
         <Title subTitle='Our PROGRAM' title='What We Offer' />
       <Programs />
       <About />
+      <Title subTitle='Gallery' title='Campus Photos' />
+      <Campus />
+      <Title subTitle='TESTIMONIALS' title='What Student Says' />
+      <Testimonials />
+      <Title subTitle='Contact Us' title='Get in Touch' />
+      <Contact />
 
       </div>
       
---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import './Contact.css'
+import msg_icon from '../../assets/msg-icon.png'
+import mail_icon from '../../assets/mail-icon.png'
+import phone_icon from '../../assets/phone-icon.png'
+import location_icon from '../../assets/location-icon.png'
+import white_arrow from '../../assets/white-arrow.png'
+
+const Contact = () => {
+  return (
+    <div className="contact">
+        <div className="contact-col">
+            <h3>Send us a message <img src={msg_icon} alt="" /></h3>
+            <p>Feel free to reach out through contatct form or find our contact information below. Your feedback, questions, and suggestions are important to us as we strive to provide exceptional service to our university community.</p>
+            <ul>
+                <li><img src={mail_icon} alt="" />contact@LXWINK</li>
+                <li> <img src={phone_icon} alt="" />+1 9097521468</li>
+                <li> <img src={location_icon} alt="" />15856 willshire Dr <br />CA 92456, United States</li>
+            </ul>
+        </div>
+        <div className="contact-col">
+            <form >
+                <label >Your name</label>
+                <input type="text" name='name' placeholder='Enter your name' required />
+                <label >Phone Number</label>
+                <input type="tel" name='phone' placeholder='Enter your mobile number' required />
+                <label >Write your message here</label>
+                <textarea name="message" rows='6' placeholder='Enter your message' required></textarea>
+                <button type='submit' className='btn dark-btn'>Submit Now <img src={white_arrow} alt="" /></button>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Contact
